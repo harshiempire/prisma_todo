98a7a1226796a2113789bab7887873e7d8dbafcf
Harshith Alle <harshithalle@Harshiths-MacBook-Air.local>
2024-03-24 16:30:54 +0530

    this is the code of just prisma

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(
+  username: string,
+  password: string,
+  firstName: string,
+  lastName: string
+) {
+  const res = await prisma.user.create({
+    data: {
+      email: username,
+      password,
+      firstname: firstName,
+      lastname: lastName,
+    },
+  });
+  console.log(res);
+}
+
+interface UpdateParams {
+  firstName: string;
+  lastName: string;
+}
+
+async function updateUser(
+  username: string,
+  { firstName, lastName }: UpdateParams
+) {
+  const res = await prisma.user.update({
+    where: { email: username },
+    data: {
+      firstname: firstName,
+      lastname: lastName,
+    },
+  });
+  console.log(res);
+}
+
+async function getUser() {
+  const res = await prisma.user.findMany();
+  console.log(res);
+}
+
+// insertUser("harshith3","harshi123","harshi3","alle3")
+// updateUser("harshith1",{firstName:"harshi1",lastName:"malle1"})
+getUser();
