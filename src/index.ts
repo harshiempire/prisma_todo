da36eafef28a353ea9883bd3319e0dd004b2d62f
Harshith Alle <harshithalle@Harshiths-MacBook-Air.local>
2024-03-27 01:08:24 +0530

    added comments and commiting

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,20 @@ import { get } from "http";
 
 const prisma = new PrismaClient();
 
+/**
+ * Asynchronously inserts a new user into the database.
+ * 
+ * This function takes an object containing the user's username, password,
+ * first name, and last name, and uses the Prisma ORM to create a new user
+ * entry in the database. The `username` is stored as the user's email.
+ * 
+ * @param {Object} userDetails - The details of the user to insert.
+ * @param {string} userDetails.username - The username (email) of the user.
+ * @param {string} userDetails.password - The password of the user.
+ * @param {string} userDetails.firstName - The first name of the user.
+ * @param {string} userDetails.lastName - The last name of the user.
+ * @returns {Promise<Object>} A promise that resolves with the result of the user creation operation.
+ */
 async function insertUser({
   username,
   password,
@@ -16,10 +30,10 @@ async function insertUser({
 }) {
   const res = await prisma.user.create({
     data: {
-      email: username,
-      firstname: firstName,
-      lastname: lastName,
-      password: password,
+      email: username, // User's email, derived from the username parameter
+      firstname: firstName, // User's first name
+      lastname: lastName, // User's last name
+      password: password, // User's password (Note: Storing plain-text passwords is not recommended)
     },
   });
   return res;
