6f6cd0c009827556b47ca0b228b70433d2d2bc4e
harshiempire <alleharshith@gmail.com>
2024-03-26 22:43:59 +0530

    changes related to the typescript express and zod

--- src/expressify.ts
+++ src/expressify.ts
@@ -1,20 +1,48 @@
 import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 const prisma = new PrismaClient();
 const app = express();
 const port = "3000";
 
+const signUpZod = z.object({
+  email: z.string().email(),
+  firstname: z.string(),
+  lastname: z.string(),
+  password: z.string(),
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.send("hi");
 });
 
-app.get("/signup",async(req: Request, res: Response) => {
-   const result = await prisma.user.findFirst({
-    where:{
-        
-    }
-   })
+app.get("/signup", async (req: Request, res: Response) => {
+  const { success } = signUpZod.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({
+      message: "Email already taken / Incorrect inputs",
+    });
+  }
+  try {
+    const result = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        password: req.body.password,
+      },
+    });
+    console.log(result);
+    res.status(200).json({
+      message: "User Created",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(411).json({
+      message: "Email already taken / Incorrect inputs",
+    });
+  }
 });
 
 app.listen(port, () => {
